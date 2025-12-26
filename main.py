f9abbd27ebf2915f96a5d42328b2ef9215277436
Peter Neubauer <development@smeky.de>
2025-12-26 12:30:58 +0100

    add environment

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import os
+from dotenv import load_dotenv
+
 def main():
     print("Hello from hetzner-dyndns!")
+    load_dotenv()
+    api_key = os.getenv('API_KEY')
+    hetzner_api = os.getenv('HETZNER_API')
+    hetzner_zone_id = os.getenv('HETZNER_ZONE_ID')
+    domain = os.getenv('DOMAIN')
 
+    print(f"Accessing HETZNER API {hetzner_api} for ZONE ID {hetzner_zone_id} to update DOMAIN {domain}")
 
 if __name__ == "__main__":
     main()
